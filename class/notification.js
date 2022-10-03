66e335a8dffc6ac131f36049f700290852d21cf2
Jesus David Rodriguez Burbano <jesus.rodriguez2@u.icesi.edu.co>
2022-10-03 17:54:31 -0500

    primera version notificaciones

---
+++ class/notification.js
@@ -0,0 +1,12 @@
+import { number, string } from "prop-types";
+
+class notification {
+
+    contructor(m, g){
+        date = Date.now;
+        msg: string = m;
+        group: number = g;
+        state = false;
+    }
+
+}
---
+++ pages/notificationPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Notification from '../componets/Notification';
+
+export default function notificationPage() {
+  return (
+    <div>
+      <Notification></Notification>
+    </div>
+  )
+}
