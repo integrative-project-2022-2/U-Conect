aa55c649fa4628427c02bbaf21adfe101a045c59
JCamiloRamirezTabares <juancaram21@gmail.com>
2022-10-08 17:56:05 -0500

    Commit

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,14 @@
+import Head from "next/head";
+
 export default function home() {
   return (
     <div>
-      
+      <Head>
+        <title>Pagina principal</title>
+      </Head>
+      <header>
+        
+      </header>
     </div>
   )
 }
