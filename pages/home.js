182824b171afd5775d06c2fbc62e829f79d89598
JCamiloRamirezTabares <juancaram21@gmail.com>
2022-10-09 21:09:30 -0500

    Home_Page V.2

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,31 @@
+import React from "react"
+import { useState } from "react"
 import HomeLayout from "../components/HomeLayout"
-import JoinConsole from "/pages/Counseling_Activities/JoinCounseling"
-
+import JoinCounseling from "/pages/Counseling_Activities/JoinCounseling"
 
 export default function home() {
+
+
+  const [children, setChildren] = React.useState(null)
+  const [button] = useState({
+    name: [
+      "New Group",
+      "Join to Advisory",
+      "Leave Advisory"
+    ],
+    fun: [join]
+  })
+
+  function join(){
+    setChildren(<JoinCounseling/>)
+  }
+
   return(
     <div>
-      <HomeLayout>
-        <JoinConsole></JoinConsole>
+      <HomeLayout
+      button = {button}
+      >
+        {children}
       </HomeLayout>
     </div>
   )
