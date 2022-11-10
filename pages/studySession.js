905141469cf2d674e413ed24871b72ae47095e05
juanpabblo16 <juanpabblo789@gmail.com>
2022-11-10 12:16:44 -0500

    transfer to next

---
+++ pages/studySession.js
@@ -0,0 +1,36 @@
+function studyjs() {
+   return <div className="">
+
+        <section className="header">
+            <nav>
+                <a href="index.html"><img src="images/logo-icesi.png" alt=""></a>
+                <div className="nav-links">
+                    <ul>
+                        <li><a href="file:///C:/Users/juanp/OneDrive/Escritorio/studySesions/index2.html">Crear sala</a>
+                        </li>
+                        <li><a href="file:///C:/Users/juanp/OneDrive/Escritorio/studySesions/index3.html">Crear espacio</a>
+                        </li>
+                        <li><a href="file:///C:/Users/juanp/OneDrive/Escritorio/studySesions/index4.html">Entrar</a></li>
+                        <li><a href="file:///C:/Users/juanp/OneDrive/Escritorio/studySesions/index5.html">Espacios</a></li>
+                        <li><a href="https://www.icesi.edu.co/moodle/my/">Perfil</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </nav>
+
+            <div className="text-box">
+                <h1>Sistema de agregar salas de estudio</h1>
+                <p>Para agregar una sala de estudio, debe estar disponible el espacio y contar con el tiempo en el rango
+                    escogido.
+                </p>
+                <a href="https://www.icesi.edu.co/biblioteca/">Visita la biblioteca para mas informacion</a>
+            </div>
+        </section>
+
+
+    </div>
+}
+
+
