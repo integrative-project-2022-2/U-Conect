06d5460a9652d87239912b96db8c11555dc183b0
JuanFCastillo <1006071586@u.icesi.edu.co>
2022-11-28 11:51:48 -0500

    US Juan Pablo

--- pages/Counseling_Activities/asesoria.js
+++ pages/Counseling_Activities/asesoria.js
@@ -32,19 +32,19 @@ export default function Home() {
           <input type="groupName" class="form-control" id="Input-groupName" />
         </div>
 
-        <div className="mb-3">
-          <label for="Inputmanager" className="form-label">Selecciona una materia </label>
-          <select className="form-select" aria-label="Default select example">
-            <option selected>Materia</option>
-            <option value="1">Algebra y funciones</option>
-            <option value="2">COE1</option>
-            <option value="3">Organizaciones</option>
-          </select>
+        <div class="mb-3">
+          <label for="Input-groupName" class="form-label">Fecha de la asesoria</label>
+          <input type="groupName" class="form-control" id="Input-groupName" />
         </div>
 
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Integrantes </label>
-          <Chipa></Chipa>
+        <div class="mb-3">
+          <label for="Input-groupName" class="form-label">Empieza a las</label>
+          <input type="groupName" class="form-control" id="Input-groupName" />
+        </div>
+
+        <div class="mb-3">
+          <label for="Input-groupName" class="form-label">Termina a las</label>
+          <input type="groupName" class="form-control" id="Input-groupName" />
         </div>
 
         <div className="mb-3">
@@ -57,21 +57,43 @@ export default function Home() {
         </div>
 
 
-        {/* <div className="mb-3">
-  <label for="InputgroupId" className="form-label">ADICIONALES</label>
-</div>
-<div className="mb-3">
-  <label for="InputgroupId" className="form-label">Codigo de grupo </label>
-  <input type="groupId" className="form-control" />
-</div>
-<div className="mb-3">
-  <label for="Inputmanager" className="form-label">Profesor </label>
-  <input type="manager" className="form-control" />
-</div>
-<div className="mb-3">
-  <label for="Input-owner_id" className="form-label">Propietario del grupo </label>
-  <input type="owner_id" className="form-control" />
-</div> */}
+        <div className="mb-3">
+          <label for="Inputmanager" className="form-label">Tipo de asesoría</label>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Estado</option>
+            <option value="PUBLICO">MONITORIA</option>
+            <option value="PRIVADO">ASESORIA</option>
+          </select>
+        </div>
+
+        <div className="mb-3">
+          <label for="Inputmanager" className="form-label">Medio</label>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Estado</option>
+            <option value="PUBLICO">PRESENCIAL</option>
+            <option value="PRIVADO">VIRTUAL</option>
+          </select>
+        </div>
+
+        <div className="mb-3">
+          <label for="Inputmanager" className="form-label">Responsable</label>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Profesor y/o Monitor</option>
+            <option value="Emily">Emily</option>
+          </select>
+        </div>
+
+
+        <div className="mb-3">
+          <label for="Input-subject" className="form-label">Selecciona una materia </label>
+          <select className="form-select" aria-label="Default select example" name="subject">
+            <option selected>Materia</option>
+            <option value="10332">Proyecto integrador 1</option>
+            <option value="10255">Inferencia estadística</option>
+          </select>
+        </div>
+
+
 
         <button type="submit" className="btn" id={styles.botoncito} onClick={() => mostrar()}>Aceptar</button>
 
